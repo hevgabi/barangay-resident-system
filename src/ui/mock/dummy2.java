b6e144ad506135cffae54a7f444a795099b46871
klawi1121 <qcaoile@gmail.com>
2026-02-15 13:19:58 +0800

    added new method

--- src/ui/mock/dummy2.java
+++ src/ui/mock/dummy2.java
@@ -4,4 +4,9 @@ public class dummy2{
     public void method(){
         System.out.println("Hello world");
     }
+
+    public void dohtem(){
+        System.out.println("dlrow olleh");
+    }
 }
+
